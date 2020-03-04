c0d69b4dd2694c8dfb976c7b502a06c7af6450ea
Russell Esperanza <resperanza11@gmail.com>
2020-03-03 16:50:06 -0800

    Completed Palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -156,6 +156,17 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase();
+  
+  if (string.length <= 1) {
+    return true;
+  } else {
+    if (string[0] === string[string.length - 1]) {
+      return palindrome(string.slice(1, string.length - 1));
+    } else {
+      return false;
+    }
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
